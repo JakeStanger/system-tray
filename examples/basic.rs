698737c91bbde6b93c6134441c1df5c1be758cb9
ogios <ogios@foxmail.com>
2024-11-17 20:57:57 +0800

    drop unused variable initial_items

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,10 +5,10 @@ async fn main() {
     let client = Client::new().await.unwrap();
     let mut tray_rx = client.subscribe();
 
-    #[allow(unused_variables)]
     let initial_items = client.items();
 
     // do something with initial items...
+    drop(initial_items);
 
     while let Ok(ev) = tray_rx.recv().await {
         println!("{ev:?}"); // do something with event...
