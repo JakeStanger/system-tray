edd684858dd97352b09a4872f547b20531b112ea
ogios <ogios@foxmail.com>
2024-11-17 17:07:56 +0800

    add tokio feature for zbus; add basic example

---
+++ examples/basic.rs
@@ -0,0 +1,15 @@
+use system_tray::client::Client;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new().await.unwrap();
+    let mut tray_rx = client.subscribe();
+
+    let initial_items = client.items();
+
+    // do something with initial items...
+
+    while let Ok(ev) = tray_rx.recv().await {
+        println!("{ev:?}"); // do something with event...
+    }
+}
