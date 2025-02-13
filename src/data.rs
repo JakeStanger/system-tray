fe4227d650be3c6d917d03eb6cb93e50147d624b
ogios <ogios@foxmail.com>
2025-02-13 15:18:10 +0800

    less duplicate code

--- src/data.rs
+++ src/data.rs
@@ -1,101 +1,71 @@
+use crate::{item::StatusNotifierItem, menu::TrayMenu};
+use std::sync::{Arc, Mutex};
+
 #[cfg(feature = "data")]
-pub use data_all::*;
+pub type BaseMap = std::collections::HashMap<String, (StatusNotifierItem, Option<TrayMenu>)>;
 
 #[cfg(not(feature = "data"))]
-pub use data_destination_only::TrayItemMap;
+type BaseMap = std::collections::HashSet<String>;
 
-#[cfg(feature = "data")]
-mod data_all {
-    use crate::{item::StatusNotifierItem, menu::TrayMenu};
-    use std::{
-        collections::HashMap,
-        sync::{Arc, Mutex},
-    };
-    use tracing::error;
-
-    pub type BaseMap = HashMap<String, (StatusNotifierItem, Option<TrayMenu>)>;
-
-    #[derive(Debug, Clone)]
-    pub struct TrayItemMap {
-        inner: Arc<Mutex<BaseMap>>,
-    }
-    impl TrayItemMap {
-        pub(crate) fn get_map(&self) -> Arc<Mutex<BaseMap>> {
-            self.inner.clone()
-        }
-
-        pub(crate) fn new() -> Self {
-            Self {
-                inner: Arc::new(Mutex::new(HashMap::new())),
-            }
-        }
-        pub(crate) fn new_item(&self, dest: String, item: &StatusNotifierItem) {
-            self.inner
-                .lock()
-                .expect("mutex lock should succeed")
-                .insert(dest, (item.clone(), None));
-        }
-        pub(crate) fn remove_item(&self, dest: &str) {
-            self.inner
-                .lock()
-                .expect("mutex lock should succeed")
-                .remove(dest);
-        }
-        pub(crate) fn update_menu(&self, dest: &str, menu: &TrayMenu) {
-            if let Some((_, menu_cache)) = self
-                .inner
-                .lock()
-                .expect("mutex lock should succeed")
-                .get_mut(dest)
-            {
-                menu_cache.replace(menu.clone());
-            } else {
-                error!("could not find item in state");
-            }
-        }
-        pub(crate) fn clear_items(&self) -> Vec<String> {
-            let mut items = self.inner.lock().expect("mutex lock should succeed");
-            items.drain().map(|(k, _)| k).collect()
-        }
-    }
+#[derive(Debug, Clone)]
+pub(crate) struct TrayItemMap {
+    inner: Arc<Mutex<BaseMap>>,
 }
 
-#[cfg(not(feature = "data"))]
-mod data_destination_only {
-    use crate::{item::StatusNotifierItem, menu::TrayMenu};
-    use std::{
-        collections::HashSet,
-        sync::{Arc, Mutex},
-    };
-
-    #[derive(Debug, Clone)]
-    pub struct TrayItemMap {
-        inner: Arc<Mutex<HashSet<String>>>,
+impl TrayItemMap {
+    pub(crate) fn new() -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(Default::default())),
+        }
     }
-    impl TrayItemMap {
-        pub(crate) fn new() -> Self {
-            Self {
-                inner: Arc::new(Mutex::new(HashSet::new())),
+
+    #[cfg(feature = "data")]
+    pub(crate) fn get_map(&self) -> Arc<Mutex<BaseMap>> {
+        self.inner.clone()
+    }
+
+    pub(crate) fn new_item(&self, dest: String, item: &StatusNotifierItem) {
+        let mut lock = self.inner.lock().expect("mutex lock should succeed");
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "data")] {
+                lock.insert(dest, (item.clone(), None));
+            }else {
+                let _ = item;
+                lock.insert(dest);
             }
         }
-        pub(crate) fn new_item(&self, dest: String, _: &StatusNotifierItem) {
-            self.inner
-                .lock()
-                .expect("mutex lock should succeed")
-                .insert(dest);
+    }
+
+    pub(crate) fn remove_item(&self, dest: &str) {
+        self.inner
+            .lock()
+            .expect("mutex lock should succeed")
+            .remove(dest);
+    }
+
+    pub(crate) fn clear_items(&self) -> Vec<String> {
+        let mut lock = self.inner.lock().expect("mutex lock should succeed");
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "data")] {
+                lock.drain().map(|(k, _)| k).collect()
+            }else {
+                lock.drain().collect()
+            }
         }
-        pub(crate) fn remove_item(&self, dest: &str) {
-            self.inner
-                .lock()
-                .expect("mutex lock should succeed")
-                .remove(dest);
-        }
-        pub(crate) fn update_menu(&self, _: &str, _: &TrayMenu) {
-            // WE DO NOTHING HERE
-        }
-        pub(crate) fn clear_items(&self) -> Vec<String> {
-            let mut items = self.inner.lock().expect("mutex lock should succeed");
-            items.drain().collect()
+    }
+
+    pub(crate) fn update_menu(&self, dest: &str, menu: &TrayMenu) {
+        cfg_if::cfg_if! {
+            if #[cfg(feature = "data")] {
+                if let Some((_, menu_cache)) = self.inner.lock().unwrap().get_mut(dest) {
+                    menu_cache.replace(menu.clone());
+                } else {
+                    tracing::error!("could not find item in state");
+                }
+            }else {
+                let _ = menu;
+                let _ = dest;
+            }
         }
     }
 }
